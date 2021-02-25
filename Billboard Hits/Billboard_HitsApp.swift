0807dc262c0c5632a5d640042738d9b5f212c26d
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-25 11:41:36 -0600

    Initial Commit

---
+++ Billboard Hits/Billboard_HitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Billboard_HitsApp.swift
+//  Billboard Hits
+//
+//  Created by Tom Bredemeier on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Billboard_HitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
